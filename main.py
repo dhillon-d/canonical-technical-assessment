e881bfe7cf89018d17cef2375e6b71140e099785
Dilraj Dhillon <dilrajdhillon@gmail.com>
2023-04-30 13:50:55 -0700

    display formatted top packages

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import gzip
 import argparse
 import sys
 
+# set up command line options
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "architecture", help="For a given architecture, display the top 10 packages with the  most files associated with them. For example 'python main.py amd64', see a full list at 'http://ftp.uk.debian.org/debian/dists/stable/main/'.")
 args = parser.parse_args()
 
+# download contents index showing files associated to packages
 url = f'http://ftp.uk.debian.org/debian/dists/stable/main/Contents-{args.architecture}.gz'
-
 response = requests.get(url, stream=True)
 try:
     response.raise_for_status()
@@ -18,10 +19,11 @@ except requests.exceptions.HTTPError as e:
     sys.exit(1)
 content = response.raw
 
+# read response into memory
 with gzip.open(content, 'rt') as f:
     lines = f.readlines()
 
-
+# parse response for top packages
 numberOfFilesInPackages = {}
 TOP_PACKAGES = 10
 for line in lines:
@@ -33,4 +35,14 @@ for line in lines:
         else:
             numberOfFilesInPackages[package] = 1
 sortedTopPackages = dict(sorted(numberOfFilesInPackages.items(), key=lambda x: x[1], reverse=True)[:TOP_PACKAGES])
-print(sortedTopPackages)
+
+# display formatted top packages
+KEY_TITLE = 'PACKAGES'
+VALUE_TITLE = 'NUMBER OF FILES'
+max_key_length = max([len(key) for key in sortedTopPackages.keys()])
+max_value_length = max([len(str(value)) for value in sortedTopPackages.values()])
+max_value_length = max(max_value_length, len(VALUE_TITLE))
+print(f"{KEY_TITLE:<{max_key_length}} {VALUE_TITLE:>{max_value_length}}")
+print("-" * (max_key_length + max_value_length + 1))
+for key, value in sortedTopPackages.items():
+    print(f"{key:<{max_key_length}} {value:>{max_value_length}}")
